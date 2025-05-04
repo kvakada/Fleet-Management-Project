430cd9aaefe8019bf04f2523c5215c5206907b8f
kvakada <kvakada@umd.edu>
2025-05-04 19:24:37 -0400

    Update downtimes.py

--- apps/downtimes.py
+++ apps/downtimes.py
@@ -1,6 +1,6 @@
 import dash_core_components as dcc
 import dash_html_components as html
-from dash import dash_table
+import dash_table
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.graph_objects as go
